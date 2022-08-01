f3a6395b70b679cd338fd85cf450ce08f0cdb1c5
Pedro Q <96138011+pequepo@users.noreply.github.com>
2022-08-01 18:05:36 +0200

    first commit

---
+++ Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+public class Main {
+    public static void main(String[]args){
+        int a=10;
+        int b=30;
+        int c=33;
+    System.out.println(suma(a,b,c));
+    }
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+}
---
+++ Main/Main2.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class Main2 {
+    public static void main(String[]Args){
+        Car myCar = new Car();
+        for (int i=0; i>5; i++);
+        System.out.print(myCar.doors);
+    }
+public static int suma(int a, int b){
+    return a + b;
+}
+}
+class Car {
+    public int doors = 0;
+    public void AddDoor(){
+        this.doors++;
+    }
+}
